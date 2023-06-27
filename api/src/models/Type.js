f1b6d718330e7473be8f328edae3232dba8b3e06
camiloduquee <camiloduquee@gmail.com>
2023-06-27 17:31:42 -0500

    models data Base

--- api/src/models/Type.js
+++ api/src/models/Type.js
@@ -5,9 +5,10 @@ module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('type', {
     id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey:true
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        allowNull: false,
+        primaryKey: true,
     },
     name: {
       type: DataTypes.STRING,
