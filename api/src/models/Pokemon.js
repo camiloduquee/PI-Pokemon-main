e8c0c6ad935bdd3a294399a6753dcae22af5d938
camiloduquee <camiloduquee@gmail.com>
2023-06-27 17:31:43 -0500

    models data base

--- api/src/models/Pokemon.js
+++ api/src/models/Pokemon.js
@@ -17,23 +17,26 @@ module.exports = (sequelize) => {
       type: DataTypes.STRING,
       allowNull: false
     },
-    life: {
-      
+    hp: {
+      type: DataTypes.INTEGER,
+      allowNull: false
     },
     attack: {
-
+      type: DataTypes.INTEGER,
+      allowNull: false
     },
     defense: {
-
+      type: DataTypes.INTEGER,
+      allowNull: false
     },
     speed: {
-
+      type: DataTypes.INTEGER
     },
     height: {
-
+      type: DataTypes.INTEGER
     },
     weight: {
-
+      type: DataTypes.INTEGER
     }
     
   },{ timestamps: false });
