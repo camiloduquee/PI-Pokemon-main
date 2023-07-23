bf45572c0c8bb7277577de77941c919d42c76dc1
camiloduquee <camiloduquee@gmail.com>
2023-07-23 15:53:58 -0500

    lading page ok

--- client/src/views/Landing/Landing.jsx
+++ client/src/views/Landing/Landing.jsx
@@ -1,13 +1,27 @@
 import { useNavigate } from "react-router-dom";
+import style from "./Landing.module.css";
+import logPokemon from "../../assets/img/LogoSombra.png";
+import picachuImg from "../../assets/img/picachu.png";
 const Landing = () => {
   const navigation = useNavigate();
   const handleGoToHome = () => {
     navigation("/home");
   };
   return (
-    <div>
-      <h1>Esta es la vista de Landing</h1>
-      <button onClick={handleGoToHome}>Home</button>
+    <div className={style.container}>
+      <div className={style.borderRight}></div>
+      <div className={style.containerInterior}>
+        <img src={logPokemon} className={style.image} />
+        <div className={style.circulo}>
+        <button className={style.button} onClick={handleGoToHome}>
+          Home
+        </button>
+
+        </div>
+       
+        <img src={picachuImg} className={style.imagePicachu}/>
+      </div>
+      <div className={style.borderLeft} ></div>
     </div>
   );
 };
